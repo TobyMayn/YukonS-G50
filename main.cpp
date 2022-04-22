3a834847178fe00b42563c1887c78c0f95a8aead
mads05dk <90009716+mads05dk@users.noreply.github.com>
2022-04-22 10:21:24 +0200

    Quick print of field
    Quick sanity check to show functionality in console

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //Test to show how it could be made in conole
+    std::cout << "\tC1\tC2\tC3\tC4\tC5\tC6\tC7" << std::endl;
+    printf("\tKC\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF1");
     return 0;
 }
