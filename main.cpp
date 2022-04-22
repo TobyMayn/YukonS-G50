3b3fe01a59319974169433b52e8c24d1b4422f21
mads05dk <90009716+mads05dk@users.noreply.github.com>
2022-04-22 10:45:23 +0200

    Changed from c++ to c notation
    One of the functions were made with c++ commands

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
 int main() {
+    int a = 4;
     //Test to show how it could be made in conole
-    std::cout << "\tC1\tC2\tC3\tC4\tC5\tC6\tC7" << std::endl;
-    printf("\tKC\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF1");
+    printf("\tC1\tC2\tC3\tC4\tC5\tC6\tC7\n\n");
+    printf("\tKC\t[]\t[]\t[]\t[]\t[]\t[]\t\tF1\n");
+    printf("\t  \t7H\t[]\t[]\t[]\t[]\t[]\t\t\n");
+    printf("\t  \t  \t5H\t[]\t[]\t[]\t[]\t\tF2\n");
+    printf("\t  \t  \t  \t6C\t[]\t[]\t[]\t\t\n");
+    printf("\t  \t  \t  \t  \t6S\t[]\t[]\t\tF3\n");
+    printf("\t  \t  \t  \t  \t  \tQC\t[]\t\t\n");
+    printf("\t  \t  \t  \t  \t  \t  \tKH\t\tF4\n");
+    printf("\t  \t  \t  \t  \t  \t  \t  \t\t\n");
     return 0;
 }
